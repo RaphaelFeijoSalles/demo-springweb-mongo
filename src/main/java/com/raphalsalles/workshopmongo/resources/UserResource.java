d04a4e87bf13cc1a2a28a5fc122fbc93da10c257
Raphael Salles <feijoraphael1@gmail.com>
2025-12-18 12:48:59 -0300

    DTO patter for return Users

--- src/main/java/com/raphalsalles/workshopmongo/resources/UserResource.java
+++ src/main/java/com/raphalsalles/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.raphalsalles.workshopmongo.resources;
 
 import com.raphalsalles.workshopmongo.domain.User;
+import com.raphalsalles.workshopmongo.dto.UserDTO;
 import com.raphalsalles.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,8 +20,10 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        //instancia na lista cada userdto com um user, conforme construtor do userdto
+        List<UserDTO> listDTO = list.stream().map(UserDTO::new).toList();
+        return ResponseEntity.ok().body(listDTO);
     }
 }
