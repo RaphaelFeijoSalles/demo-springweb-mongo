df55c1df38a86324bd3c8c3bb2b52061dc160144
Raphael Salles <feijoraphael1@gmail.com>
2025-12-22 12:17:28 -0300

    retrieving Post by id

---
+++ src/main/java/com/raphalsalles/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.raphalsalles.workshopmongo.services;
+
+import com.raphalsalles.workshopmongo.domain.Post;
+import com.raphalsalles.workshopmongo.repository.PostRepository;
+import com.raphalsalles.workshopmongo.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = postRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+
+    }
+
+}
