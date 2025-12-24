1918c18596187b59c7e529928c4eb6c13aeaced9
Raphael Salles <feijoraphael1@gmail.com>
2025-12-24 13:41:10 -0300

    Simple query with @Query

--- src/main/java/com/raphalsalles/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/raphalsalles/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.raphalsalles.workshopmongo.repository;
 
 import com.raphalsalles.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +10,8 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
-    List<Post> findByTitle(String text);
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String title);
 }
